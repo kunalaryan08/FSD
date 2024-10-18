c0da1570072b98d3b4e66aad351e0413ce2dd4b7
kunalaryan08 <kunalaryan04@gmail.com>
2024-10-18 15:42:20 +0530

    initial commit

---
+++ JS WORK/index.js
@@ -0,0 +1,41 @@
+// console.log("Hello World");
+// let a=10;
+// console.log(a);
+
+// function solve(){
+//     console.log("Hello")   
+// }
+// solve();
+
+// function cCompiler(){
+//     return "c compiler selected";
+// }
+
+// function javaCompiler(){
+//     return "java compiler selected";
+// }
+
+// function selectLanguage(clbk){
+//     console.log("Hi, You are Selected " + clbk);
+// }
+
+// selectLanguage(javaCompiler());
+
+// function selectLanguage(lang){
+//     if(lang == 'c'){
+//         let data;
+//         function cCompiler(){
+//             return "c compiler selected";
+//         }
+//     }
+//         if(lang == "java"){
+//             function javaCompiler(){
+//                 return "java compiler selected";
+//             }
+//         }
+//     }
+
+console.log("Hello JS");
+console.dir(document);
+
+
